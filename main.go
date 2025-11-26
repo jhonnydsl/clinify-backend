f7e5e32003122629a3edebc73cb2cc1231b983a7
jhonnydsl <jhonny.s.l.12345@gmail.com>
2025-11-25 23:32:37 -0300

    feat: conectando ao banco de dados

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/gin-gonic/gin"
+	"github.com/jhonnydsl/clinify-backend/src/repository"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("error loading enviroment variables")
+		return
+	}
+	
+	err = repository.Connect()
+	if err != nil {
+		log.Fatalf("error connecting to the database: %v", err)
+	} else {
+		log.Println("connection estabilished")
+	}
+	defer repository.DB.Close()
+
+	app := gin.Default()
+
+	app.Run(":8080")
 }
